7b57e14c27485149badf9ca8dd26dcf89c220e02
Lukide0 <54941895+Lukide0@users.noreply.github.com>
2020-06-23 23:01:34 +0200

    created basic structure

---
+++ includes/structure/footer.php
@@ -0,0 +1,10 @@
+<div>
+  <p>
+    This is an open-source project. Visit
+    <a href="https://gradarr.github.io">this</a>
+    link to get to know more.
+  </p>
+  <p>
+    App version: 0.1
+  </p>
+</div>
